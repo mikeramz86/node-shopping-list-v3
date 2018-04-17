8a19cd0e389345d00f3a1c1d45792ffdddc66d83
Mike <mikeramzdesign@gmail.com>
2018-04-16 23:41:04 -0700

    updated lock json?

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ app.delete('/shopping-list/:id', (req, res) => {
 });
 
 
+
+
 // when new recipe added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
 // if okay, add new item, and return it with a status 201.
@@ -72,6 +74,17 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+// app.delete('.recipes/:id', (req, res)=> {
+//   Recipes.delete(req.params.id);
+//   console.log(`Delete Recipes Item \`${req.params.id}\``);
+//   res.status(204).end();
+// })
+
+app.delete('/shopping-list/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.ID}\``);
+  res.status(204).end();
+});
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
